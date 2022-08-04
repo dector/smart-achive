4a5c7e443671e8af9c55152a6ad3ed77d94e5fe1
Denys M <dector@dector.space>
2022-08-04 21:13:57 +0200

    feat: add simple `doctor` command

--- arc.fish
+++ arc.fish
@@ -1,9 +1,19 @@
 set _VERSION 0.0.0.1
 
 function arc
+    switch $argv[1]
+        case 'doctor'
+        begin
+            echo tar: (which tar)
+            echo unzip: (which unzip)
+            return
+        end
+    end
+
     argparse \
         'v/version' \
         'h/help' \
+        'x/extract' \
         -- $argv
 
     if set -q _flag_version
@@ -13,6 +23,13 @@ function arc
 
     if set -q _flag_help
         echo 'arc - archives wrapper'
+        return
+    end
+
+    echo $argv
+    if set -q _flag_extract
+        echo $argv
+        echo 'TODO'
         return
     end
 end
